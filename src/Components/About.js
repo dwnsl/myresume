fd1676d1af7545bebd3bb4cf47d01806c694f95d
Daniel Wensel <danieljwensel@gmail.com>
2021-11-03 16:05:29 -0600

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import pdf from './danresume.pdf'
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var profilepic= "images/"+this.props.data.image;
+      var bio = this.props.data.bio;
+
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+         </div>
+         <div className="nine columns main-col">
+            <h2>About Me</h2>
+
+            <p>Utah based, raised in France and born in Texas.</p>
+            <p>{bio}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   <span>{phone}</span><br />
+                     <span>{email}</span>
+					   </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                     <a href={pdf} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
